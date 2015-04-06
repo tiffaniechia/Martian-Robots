10e4d4cc52433c4766fa22b5b427129f8b14f17d
tiffaniechia <tiffaniekyrachia@gmail.com>
2015-04-06 22:49:57 +0800

    Created robot mapper

---
+++ spec/mappers/robot_mapper_spec.js
@@ -0,0 +1,12 @@
+var robot_mapper = require('../../src/mappers/robot_mapper.js');
+var Robot = require('../../src/models/robot.js');
+
+describe("robot_mapper", function(){
+    var result;
+
+    it("should return robot objects from raw robots data", function(){
+        var rawRobotsData = ["1 1 E LF", "3 2 N LF"];
+        result = robot_mapper.map(rawRobotsData);
+        expect(result).toEqual([new Robot(1, 1, "E", "LF"), new Robot(3, 2, "N", "LF")]);
+    });
+});
---
+++ src/mappers/robot_mapper.js
@@ -0,0 +1,19 @@
+var Robot = require("../../src/models/robot.js");
+var  _ = require('underscore')._;
+
+var map = function (rawRobotsData) {
+    return _.map(rawRobotsData, function (rawRobotData) {
+        var data = rawRobotData.split(" ");
+        var parsedData = {
+            xAxis: parseInt(data[0]),
+            yAxis: parseInt(data[1]),
+            facingDirection: data[2],
+            instructions: data[3]
+        };
+        return new Robot(parsedData.xAxis, parsedData.yAxis, parsedData.facingDirection, parsedData.instructions);
+    });
+};
+
+module.exports = {
+    map: map
+};
