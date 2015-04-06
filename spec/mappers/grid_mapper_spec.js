699c541b3662a3e5240f9bdfbc6df2874fbdd43d
tiffaniechia <tiffaniekyrachia@gmail.com>
2015-04-07 00:58:49 +0800

    Added Error handling for Grid data

--- spec/mappers/grid_mapper_spec.js
+++ spec/mappers/grid_mapper_spec.js
@@ -1,12 +1,46 @@
 var grid_mapper = require('../../src/mappers/grid_mapper.js');
 var Grid = require('../../src/models/grid.js');
 
-describe("grid_mapper", function(){
+describe("grid_mapper", function () {
     var result;
 
-    it("should return grid object from raw grid data", function(){
-        var rawGridData = '5 3';
-        result = grid_mapper.map(rawGridData);
-        expect(result).toEqual(new Grid(5,3));
+    describe("#map", function () {
+        it("should return grid object from raw grid data", function () {
+            var rawGridData = '5 3';
+            result = grid_mapper.map(rawGridData);
+            expect(result).toEqual(new Grid(5, 3));
+        });
+    });
+
+    describe("#errors", function () {
+        describe("#isMoreThanFifty Error", function () {
+            it("should throw an error if grid data y axis is more than 50", function () {
+                expect(function () {grid_mapper.map('5 51')}).toThrow(new Error("Grid size cannot be greater than 50"));
+            });
+
+            it("should throw an error if grid data y axis is more than 50", function () {
+                expect(function () {grid_mapper.map('51 5')}).toThrow(new Error("Grid size cannot be greater than 50"));
+            });
+        });
+
+        describe("#isLessThanZero Error", function () {
+            it("should throw an error if grid data x axis less than 0", function () {
+                expect(function () {grid_mapper.map('-1 5')}).toThrow(new Error("Grid size cannot be less than 0"));
+            });
+
+            it("should throw an error if grid data y axis less than 0", function () {
+                expect(function () { grid_mapper.map('5 -1')}).toThrow(new Error("Grid size cannot be less than 0"));
+            });
+        });
+
+        describe("#isNotANumber Error", function () {
+            it("should throw an error if grid data x axis not a number", function () {
+                expect(function () {grid_mapper.map('k 5')}).toThrow(new Error("Grid size must be a number"));
+            });
+
+            it("should throw an error if grid data y axis not a number", function () {
+                expect(function () {grid_mapper.map('5 k')}).toThrow(new Error("Grid size must be a number"));
+            });
+        });
     });
 });
--- src/mappers/grid_mapper.js
+++ src/mappers/grid_mapper.js
@@ -1,10 +1,29 @@
 var Grid = require("../../src/models/grid.js");
-var  _ = require('underscore')._;
+var _ = require('underscore')._;
 
 var map = function (rawGridData) {
     rawGridData = rawGridData.split(" ");
     var data = {xAxis: parseInt(rawGridData[0]), yAxis: parseInt(rawGridData[1])};
+    checkForErrors(data.xAxis, data.yAxis);
     return new Grid(data.xAxis, data.yAxis);
+};
+
+var isMoreThanFiftyCheck = function (xAxis, yAxis) {
+    if (xAxis > 50 || yAxis > 50) throw new Error("Grid size cannot be greater than 50");
+};
+
+var isLessThanZeroCheck = function (xAxis, yAxis) {
+    if (xAxis < 0 || yAxis < 0) throw new Error("Grid size cannot be less than 0");
+};
+
+var isNotANumberCheck = function (xAxis, yAxis) {
+    if (isNaN(xAxis) || isNaN(yAxis)) throw new Error("Grid size must be a number");
+};
+
+var checkForErrors = function (xAxis, yAxis) {
+    isMoreThanFiftyCheck(xAxis, yAxis);
+    isLessThanZeroCheck(xAxis, yAxis);
+    isNotANumberCheck(xAxis, yAxis);
 };
 
 module.exports = {
