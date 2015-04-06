6ac07200f47c3dfb3b8e2370c5ab3031727206bd
tiffaniechia <tiffaniekyrachia@gmail.com>
2015-04-07 01:25:11 +0800

    Added Error Handling for Robot Data

--- spec/mappers/robot_mapper_spec.js
+++ spec/mappers/robot_mapper_spec.js
@@ -1,12 +1,44 @@
 var robot_mapper = require('../../src/mappers/robot_mapper.js');
 var Robot = require('../../src/models/robot.js');
 
-describe("robot_mapper", function(){
+describe("robot_mapper", function () {
     var result;
 
-    it("should return robot objects from raw robots data", function(){
-        var rawRobotsData = ["1 1 E LF", "3 2 N LF"];
-        result = robot_mapper.map(rawRobotsData);
-        expect(result).toEqual([new Robot(1, 1, "E", ["L","F"]), new Robot(3, 2, "N", ["L","F"])]);
+    describe("#map", function () {
+        it("should return robot objects from raw robots data", function () {
+            var rawRobotsData = ["1 1 E LF", "3 2 N LF"];
+            result = robot_mapper.map(rawRobotsData);
+            expect(result).toEqual([new Robot(1, 1, "E", ["L", "F"]), new Robot(3, 2, "N", ["L", "F"])]);
+        });
+    });
+
+    describe("#errors", function () {
+        describe("#isNotANumber Error", function () {
+            it("should throw an error if robot data x axis not a number", function () {
+                expect(function () {robot_mapper.map(["k 1 E LF"])}).toThrow(new Error("Robot axis must be a number"));
+            });
+
+            it("should throw an error if robot data y axis not a number", function () {
+                expect(function () {robot_mapper.map(["1 k E LF"])}).toThrow(new Error("Robot axis must be a number"));
+            });
+        });
+
+        describe("#isNotAValidDirection Error", function () {
+            it("should throw an error if robot facing direction not N, S, E, or W", function () {
+                expect(function () {robot_mapper.map(["1 1 K LF"])}).toThrow(new Error("Robot facing direction must be N, S, E, or W"));
+            });
+        });
+
+        describe("#isNotAMovementInstruction Error", function () {
+            it("should throw an error if robot instruction not L, R, or F", function () {
+                expect(function () {robot_mapper.map(["1 1 N KK"])}).toThrow(new Error("Robot instruction must be L, R, or F"));
+            });
+        });
+
+        describe("#isMoreThanHundredInstructions", function () {
+            it("should throw an error if robot instruction more than 100 characters", function () {
+                expect(function () {robot_mapper.map(["1 1 N LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLR"])}).toThrow(new Error("Robot instructions must be less than 100"));
+            });
+        });
     });
 });
--- src/mappers/robot_mapper.js
+++ src/mappers/robot_mapper.js
@@ -1,5 +1,5 @@
 var Robot = require("../../src/models/robot.js");
-var  _ = require('underscore')._;
+var _ = require('underscore')._;
 
 var map = function (rawRobotsData) {
     return _.map(rawRobotsData, function (rawRobotData) {
@@ -10,8 +10,34 @@ var map = function (rawRobotsData) {
             facingDirection: data[2],
             instructions: data[3].split("")
         };
+        checkForErrors(parsedData.xAxis, parsedData.yAxis, parsedData.facingDirection, parsedData.instructions);
         return new Robot(parsedData.xAxis, parsedData.yAxis, parsedData.facingDirection, parsedData.instructions);
     });
+};
+
+var isNotANumberCheck = function (xAxis, yAxis) {
+    if (isNaN(xAxis) || isNaN(yAxis)) throw new Error("Robot axis must be a number");
+};
+
+var isNotAValidDirection = function (facingDirection) {
+    if (!(facingDirection == 'N' || facingDirection == 'S' || facingDirection == 'E' || facingDirection == 'W')) throw new Error("Robot facing direction must be N, S, E, or W");
+};
+
+var isNotAMovementInstruction = function (instructions) {
+    return _.each(instructions, function (instruction) {
+        if (!(instruction == 'L' || instruction == 'R' || instruction == 'F')) throw new Error("Robot instruction must be L, R, or F");
+    });
+};
+
+var isMoreThanHundredInstructions = function (instructions) {
+    if (instructions.length > 100) throw new Error("Robot instructions must be less than 100");
+};
+
+var checkForErrors = function (xAxis, yAxis, facingDirection, instructions) {
+    isNotANumberCheck(xAxis, yAxis);
+    isNotAValidDirection(facingDirection);
+    isMoreThanHundredInstructions(instructions);
+    isNotAMovementInstruction(instructions);
 };
 
 module.exports = {
