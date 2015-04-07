63b32d8daa4beb55109187b9d4f1a32759d63de5
tiffaniechia <tiffaniekyrachia@gmail.com>
2015-04-07 14:44:48 +0800

    Added console log for runtime

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var main_controller = require('./src/main_controller.js');
 var fs = require('fs');
+var chalk = require('chalk');
 
 var output = main_controller.main();
+console.log(chalk.green("Processing input.txt..."));
+
 fs.writeFileSync('output.txt',output);
+console.log(chalk.green("Successfully processed, check output.txt for results!"));
